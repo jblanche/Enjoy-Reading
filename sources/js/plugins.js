2bde7b162e64e1fd3818fe76abea3ae1b5c1e057
Jonathan Blanchet <contact@jblanche.fr>
2011-03-25 00:30:55 +0100

    website

--- sources/js/plugins.js
+++ sources/js/plugins.js
@@ -5,7 +5,7 @@ window.log = function(){
   log.history = log.history || [];   // store logs to an array for reference
   log.history.push(arguments);
   arguments.callee = arguments.callee.caller;  
-  if(this.console) /*console.log( Array.prototype.slice.call(arguments) )*/;
+  if(this.console) console.log( Array.prototype.slice.call(arguments) );
 };
 // make it safe to use console.log always
 (function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
